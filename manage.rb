add5280be87ae5bcf5286c941483e88ceebe3c68
Chris Heald <chris@polaris.localdomain>
2010-01-05 15:56:01 -0700

    Initial commit

---
+++ manage.rb
@@ -0,0 +1,74 @@
+#!/usr/bin/env ruby
+require 'murmur-interface'
+
+manager = MurmurManager.new
+
+def print_tabbed(key, val, tabs = 5)
+	key = key.to_s
+	val = val.to_s
+	puts sprintf("%s%s%s", key, "\t" * (tabs - (key.length / 8).ceil), val.to_s)
+end
+alias pt print_tabbed
+
+def print_options(server)
+	server.get_config.each do |key, val|
+		if %w"key certificate".include? key then
+			val = val.gsub(/[\r\n]/, "")[0..32] + "..."
+		end
+		pt(key, val)
+	end
+end
+
+def help
+	puts "Usage: manager.rb [opts]"
+	puts ""
+	pt "Args", "Effect"
+	pt "----", "------"
+	pt "[server-id]", "List a server's config"
+	pt "[server-id] set [key] [val]", "Set a server's config value"
+	pt "[server-id] start", "Start a server"
+	pt "[server-id] stop", "Stop a server"
+	pt "[server-id] destroy", "Permanently destroy a server"
+	pt "new", "Create a new server"
+	pt "list", "List existing servers"
+end
+
+cmd_or_server_id = ARGV[0]
+if ARGV.empty? or cmd_or_server_id == nil or cmd_or_server_id == "" then
+	help
+elsif cmd_or_server_id == "list" then
+	pt "Server ID", "Running", 2
+	pt "---------", "------", 2
+	manager.list_servers.each do |serverID|		
+		server = manager.get_server(serverID)
+		pt serverID, server.running?, 2
+	end	
+elsif cmd_or_server_id.to_i != 0 then
+	cmd = ARGV[1]
+	server = manager.get_server(cmd_or_server_id)
+	if cmd.nil? then
+		print_options(server)
+	elsif cmd == "set" then
+		val = ARGV[3..-1].join(" ")
+		server[ARGV[2]] = val
+		puts "Server ID #{server.id}: #{ARGV[2]} set to #{val}"
+		server.restart!
+		puts "Server restarted"
+	elsif cmd == "start" then
+		server.start!
+	elsif cmd == "stop" then
+		server.stop!
+	elsif cmd == "destroy" then
+		server.destroy!
+	else
+		puts "Unknown command"
+	end
+elsif cmd_or_server_id == "new"
+	server = manager.create_server
+	server.get_config.each do |key, val|
+		next if %w"key certificate".include? key
+		pt(key, val)
+	end
+else
+	help
+end
---
+++ murmur-interface.rb
@@ -0,0 +1,98 @@
+$: << File.join(File.expand_path(File.dirname(__FILE__)), "dbus", "lib")
+require 'dbus'
+class MurmurServer
+	attr_reader :id
+	
+	def initialize(interface, id)
+		@interface = interface
+		@id = id
+	end
+	
+	def get_config
+		config = @interface.getDefaultConf.first
+		@config = config.merge(@interface.getAllConf(id).first)
+	end
+	
+	def [](key)
+		@interface.setConf(id, key) || get_config[key]
+	end
+	
+	def []=(key, val)
+		if key == "superuser_password" then
+			set_superuser_password(val)
+			return true
+		else
+			@interface.setConf(id, key.to_s, val.to_s)
+			get_config
+		end
+	end
+	
+	def stop!
+		@interface.stop(id)
+	end
+	
+	def running?
+		@running = @interface.isBooted(id).first
+	end
+	
+	def start!
+		@interface.start(id)
+	end
+	
+	def restart!
+		stop! if running?
+		start!
+	end
+	
+	def set_superuser_password(password)
+		raise "Please specify a password" if password.nil? or password == ""
+		@interface.setSuperUserPassword(id, password)
+	end
+	
+	def destroy!
+		stop! if running?
+		@interface.deleteServer(id)
+	end
+	
+	def method_missing(method, *args)
+		@interface.send(method, id, *args).first
+	end
+end
+
+class MurmurManager
+	def initialize
+		@servers = {}
+		@bus = DBus::SystemBus.instance
+		@service = @bus.service "net.sourceforge.mumble.murmur"
+		@object = @service.object "/"
+		@object.introspect
+		@interface = @object["net.sourceforge.mumble.Meta"]
+	end
+	
+	def get_server(id)
+		id = id.to_i
+		raise "Invalid ID" if id < 1
+		@servers[id] ||= MurmurServer.new(@interface, id)
+	end
+	
+	def list_servers(all = true)
+		method = all ? :getAllServers : :getBootedServers
+		@interface.send(method).first
+	end	
+	
+	def create_server(port = nil)
+		id = @interface.newServer.first
+		server = get_server(id)
+		if port then
+			server["port"] = port
+		end
+		return server
+	end
+
+	def stop_server(server_id)
+	end
+	
+	def introspect
+		p @object.root
+	end
+end
