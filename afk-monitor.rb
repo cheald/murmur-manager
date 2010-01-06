3f1d16b3f856031c0a0b48bae80f9a3ba90ec453
Chris Heald <chris@polaris.localdomain>
2010-01-06 08:46:00 -0700

    Fix channel idle detection

--- afk-monitor.rb
+++ afk-monitor.rb
@@ -29,12 +29,12 @@ Daemons.run_proc('murmur-afk-monitor', daemon_options) do
 			users = server.get_users
 			users.each do |id, user|
 				channel_min_list[user.channel] ||= []
-				channel_min_list[user.channel].push user.idlesecs
+				channel_min_list[user.channel].push user.idlesecs.to_i
 			end
-			channel_min_times.each {|k, v| channel_idle[k] = v.min }
+			channel_min_list.each {|k, v| channel_idle[k] = v.min }
 			users.each do |id, user|
 				puts "#{user.name} idle for #{user.idlesecs} in #{user.channel} (channel idle for #{channel_idle[user.channel]})"
-				if user.channel != afk_channel and user.idlesecs > IDLE_TIMEOUT and channel_idle[user.channel] > IDLE_TIMEOUT then
+				if user.channel != afk_channel and user.idlesecs.to_i > IDLE_TIMEOUT and channel_idle[user.channel] > IDLE_TIMEOUT then
 					user.channel = afk_channel
 					server.set_state(user)
 				end
