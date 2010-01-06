f930ea328b71c0be459bbc6645fbfc3a6fa32f32
Chris Heald <chris@polaris.localdomain>
2010-01-06 08:53:21 -0700

    Give users one pass since their last move to become active, or for the channel to become active.

--- afk-monitor.rb
+++ afk-monitor.rb
@@ -4,6 +4,7 @@ require 'interfaces/ice'
 
 IDLE_NAMES = %w"afk idle"		# The first channel matching any of these names will be the catch-all idle channel
 IDLE_TIMEOUT = 3600				# Move people to the AFK channel after one hour1
+RUN_EVERY = 300
 
 daemon_options = {
   :multiple => false,
@@ -17,6 +18,7 @@ Daemons.run_proc('murmur-afk-monitor', daemon_options) do
 	while true do
 		channel_min_list = {}
 		channel_idle = {}
+		user_last_channel = {}
 		@meta.list_servers(true).each do |server|
 			afk_channel = nil
 			server.get_channels.each do |key, channel|
@@ -34,13 +36,17 @@ Daemons.run_proc('murmur-afk-monitor', daemon_options) do
 			end
 			channel_min_list.each {|k, v| channel_idle[k] = v.min }
 			users.each do |id, user|
-				puts "#{user.name} idle for #{user.idlesecs} in #{user.channel} (channel idle for #{channel_idle[user.channel]})"
-				if user.channel != afk_channel and user.idlesecs.to_i > IDLE_TIMEOUT and channel_idle[user.channel] > IDLE_TIMEOUT then
-					user.channel = afk_channel
-					server.set_state(user)
+				# puts "#{user.name} idle for #{user.idlesecs} in #{user.channel} (channel idle for #{channel_idle[user.channel]})"				
+				if 	user_last_channel[user.id] != afk_channel and
+					user.channel != afk_channel and 
+					user.idlesecs.to_i > IDLE_TIMEOUT and 
+					channel_idle[user.channel] > IDLE_TIMEOUT then
+						user.channel = afk_channel
+						server.set_state(user)
 				end
+				user_last_channel[user.id] = user.channel
 			end
 		end
-		sleep(60)
+		sleep(RUN_EVERY)
 	end
 end
